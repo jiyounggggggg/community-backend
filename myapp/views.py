741afa5d099a37e56e9f8a360ee3b10a593ebcba
Jiyoung <poiuynumi@gmail.com>
2024-05-09 19:35:08 +0900

    .

--- myapp/views.py
+++ myapp/views.py
@@ -17,16 +17,13 @@ def message_list(request):
 @api_view(['POST'])
 def create_post(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        # author = request.user  # 현재 로그인한 사용자
-        author = 'Anonymous'
-        content = request.POST.get('content')
-        created_at = timezone.now()
-
-        # Post 모델에 데이터 저장
-        print('111:', title, author, content, created_at)
-        post = Post(title=title, author=author, content=content, created_at=created_at)
-        post.save()
-
-        # return redirect('post_list')  # 저장 후 리스트 페이지로 리디렉션 (URL 이름은 상황에 맞게 변경)
-    # return render(request, 'create_post.html')
+        serializer = PostSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            print("Validated Data: ", serializer.validated_data)
+            serializer.save()
+            
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print("Errors: ", serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
